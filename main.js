b91c21dc143291705414798083211189e18d2cca
abhishek <lonulobu@gmail.com>
2022-03-04 16:42:51 +0530

    FINAL JS

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var allCardElements =document.querySelectorAll(".card");
 
 var change =document.querySelector(".change");
-change.innerHTML = colorGenerator();
+// change.innerHTML = colorGenerator();
 // change.innerHTML = extractrgbb();
 // extractrgbb();
 
@@ -9,21 +9,15 @@ var toppart=document.querySelector(".toppart");
 var newgame=document.querySelector("#newgame");
 var easybtn=document.querySelector("#easybtn");
 var hardbtn=document.querySelector("#hardbtn");
-
+var gametext=document.querySelector(".gametext");
 var chosencolor;
-var f;
-
-
-for(var i = 0; i < allCardElements.length; i++) 
-{
-    var card = allCardElements[i];
-}
+var cardnumbers=6;
 
 // ALL card color gives color to cards
   function allcardcolor()
   {
       var arr=[];
-      for(var i=0;i<6;i++)
+      for(var i=0;i< cardnumbers;i++)
       {
           arr.push(colorGenerator());
       }
@@ -39,7 +33,8 @@ colors=allcardcolor();
 // card.style.background=colors[0];
 // extractrgbb();
 
-  for(var i = 0; i < allCardElements.length; i++) 
+// THIS FOR LOOP ASSIGNS COLOR TO CARDS BY STULE
+  for(var i = 0; i < cardnumbers; i++) 
 {
     var card = allCardElements[i];
     card.style.background=colors[i];
@@ -49,21 +44,19 @@ colors=allcardcolor();
 change.innerHTML = extractrgbb();
   function extractrgbb()
   {
-      var a=colors[(Math.floor(Math.random()*6))];
+      var a=colors[(Math.floor(Math.random()*cardnumbers))];
       chosencolor=a;
     //   var a=colors[0];
       console.log(chosencolor);
       return a;
   }
 
-  for(var i = 0; i < allCardElements.length; i++) 
+  for(var i = 0; i <cardnumbers; i++) 
 {
     var card = allCardElements[i];
     console.log("inside1");
-    f=colors[i];
     card.addEventListener("click",function(e)
     {
-        // console.log(f);
         cardClicked(this);
     });
   }
@@ -82,18 +75,19 @@ change.innerHTML = extractrgbb();
         console.log("condition checked");
         toppart.style.background=chosencolor;
 
-        for(var i = 0; i < allCardElements.length; i++) 
+        for(var i = 0; i <cardnumbers; i++) 
         {
             var card = allCardElements[i];
             card.style.visibility="visible";
             card.style.background=chosencolor;
         }
+        gametext.innerHTML = "YOU WON SIR";
       }
       else
       {
           cardE.style.visibility="hidden";
           console.log("condition checked22");
-          setTimeout( function ( ) { alert( "moo" ); }, 50 ); 
+          // setTimeout( function ( ) { alert( "moo" ); }, 50 ); 
       }
   }
 
@@ -104,3 +98,47 @@ change.innerHTML = extractrgbb();
       var blue=Math.floor(Math.random()*255);
       return "rgb(" + red + ", " + green + ", " + blue + ")";
   }
+
+
+  function easyGame()
+{
+  gametext.innerHTML = "Easy level here";
+  console.log("EASY TIME");
+  cardnumbers=3;
+  for(var i = 3; i <6; i++) 
+  {
+      var card = allCardElements[i];
+      card.style.display="none";
+      // card.style.background=chosencolor;
+  }
+  console.log("laughing at you");
+console.log(cardnumbers);
+
+var colors=[];
+colors=allcardcolor();
+for(var i = 0; i < cardnumbers; i++) 
+{
+    var card = allCardElements[i];
+    card.style.background=colors[i];
+    console.log(colors[i]);
+}
+
+change.innerHTML = extractrb();
+function extractrb()
+  {
+      var a=colors[(Math.floor(Math.random()*cardnumbers))];
+      chosencolor=a;
+    //   var a=colors[0];
+      console.log(chosencolor);
+      return a;
+  }
+for(var i = 0; i <cardnumbers; i++) 
+{
+    var card = allCardElements[i];
+    console.log("inside1");
+    card.addEventListener("click",function(e)
+    {
+        cardClicked(this);
+    });
+  }
+}
